7bab3b278499102e4f0c5944a9678dfb54d4cdc8
Steve Jensen <docwho2@gmail.com>
2025-04-27 16:19:39 -0500

    tweaks

--- cloudshell.sh
+++ cloudshell.sh
@@ -9,29 +9,27 @@ sudo yum install -y java-21-amazon-corretto-devel
 echo "Verifying Java Version..."
 java -version
 
-echo "Installing latest Maven manually..."
-
-# Pick Maven version you want
+echo "Installing latest Maven locally..."
 MAVEN_VERSION=3.9.6
-#
-# Download and install
+
+# Download and untar in your HOME directory
 curl -fsSL https://downloads.apache.org/maven/maven-3/${MAVEN_VERSION}/binaries/apache-maven-${MAVEN_VERSION}-bin.tar.gz -o maven.tar.gz
-sudo mkdir -p /opt/maven
-sudo tar -xzvf maven.tar.gz -C /opt/maven --strip-components=1
+mkdir -p $HOME/maven
+tar -xzvf maven.tar.gz -C $HOME/maven --strip-components=1
 rm maven.tar.gz
-#
-# Set up environment variables
-export M2_HOME=/opt/maven
-export MAVEN_HOME=/opt/maven
+
+# Set environment variables
+export M2_HOME=$HOME/maven
+export MAVEN_HOME=$HOME/maven
 export PATH=$M2_HOME/bin:$PATH
-#
+
 echo "Verifying Maven Version..."
 mvn -version
-#
+
 echo "Installing AWS CDK globally..."
 sudo npm install -g aws-cdk
-#
+
 echo "Verifying CDK Version..."
 cdk version
-#
+
 echo "✅ Environment ready. You can now run 'cd cdk' and 'mvn install' and 'cdk deploy ...'
